2d42b126dcfed08d750146b95115dbafc5da3a94
Ayzadaa <ayzada.baichabaeva.kk@gmail.com>
2023-03-03 23:04:57 +0600

    ad  models and new pages

---
+++ lib/components/custom_tex_field.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    Key? key,
+    this.hinText,
+    this.controller,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.onTap,
+    this.maxLines,
+  }) : super(key: key);
+  final String? hinText;
+  final TextEditingController? controller;
+  final void Function()? onTap;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final int? maxLines;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      onTap: onTap,
+      maxLines: maxLines,
+      decoration: InputDecoration(
+        hintText: hinText ?? '',
+        prefixIcon: prefixIcon,
+        suffixIcon: suffixIcon,
+        border: const OutlineInputBorder(),
+      ),
+    );
+  }
+}
